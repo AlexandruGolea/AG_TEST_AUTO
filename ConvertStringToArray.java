5371c6b9349f508d1cf82bda3f64f2d7ca1e2a73
AlexandruGolea <goleaalex@gmail.com>
2020-06-17 10:33:55 +0300

    Commit1

---
+++ ConvertStringToArray.java
@@ -0,0 +1,8 @@
+public class ConvertStringToArray {
+    // 8)
+    public static void main(String[] args) {
+        String strName = "Welcome";
+        String[] strArray = new String[]{strName};
+        System.out.println(strArray[0]);
+    }
+}
